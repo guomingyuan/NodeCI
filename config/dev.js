09d968fc24a6ab083276830404357d8ba128a306
guomingyuan <33354415+guomingyuan@users.noreply.github.com>
2021-04-22 19:48:22 -0400

    Delete dev.js

--- config/dev.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  googleClientID:
-    '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
-  googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
-  mongoURI:
-    'mongodb://guoming:work123@cluster0-shard-00-00.0ve3m.mongodb.net:27017,cluster0-shard-00-01.0ve3m.mongodb.net:27017,cluster0-shard-00-02.0ve3m.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority',
-  cookieKey: '123123123',
-  redisUrl: 'redis://127.0.0.1:6379',
-}
